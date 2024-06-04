17fd3e7af05da12148f77bbe4ea40e4388e2bb88
gamename <tennis.n.smith@gmail.com>
2024-06-04 01:31:25 -0500

    implemented mac-based hostnaming

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -9,6 +9,14 @@
 #include "wifi_manager.h"
 #include "http_manager.h"
 
+/*
+Todo: Save snapshot to SD card on positive detection
+Todo: Use mac address for identification
+Todo: Include hostname in the POST request
+Todo: Show a message on the display when a cat face is detected or maybe the snapshot?
+Todo: Add a button to the device to manually trigger the doorbell ring?
+Todo: Show real-time video feed on the display (possible?)
+*/
 #define LOG_TAG "app_main"
 #define TEN_MINUTES_MICROSECONDS 600000000L // 10 minutes in microseconds
 
@@ -21,10 +29,9 @@ int64_t last_post_time = -TEN_MINUTES_MICROSECONDS;  // Initialize to a value th
 extern "C" void app_main()
 {
     ESP_LOGI(LOG_TAG,"Initializing NVS...");
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    if (esp_err_t ret = nvs_flash_init(); ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
+        nvs_flash_init();
     }
 
     ESP_LOGI(LOG_TAG,"Initializing WiFi...");
@@ -42,7 +49,11 @@ extern "C" void app_main()
 
     std::string macAddress = wifiManager.getMacAddress();
 
+    config.set_context(macAddress.c_str());
+
     std::string hostName = config.get("wifi_hostname");
+
+    config.reset_context();
 
     wifiManager.setHostname(hostName.c_str());
 
@@ -73,7 +84,7 @@ extern "C" void app_main()
     xQueueAIFrame = xQueueCreate(2, sizeof(camera_fb_t *));
     xQueueResult = xQueueCreate(1, sizeof(bool));  // Create a queue for boolean results
 
-    register_camera(PIXFORMAT_RGB565, FRAMESIZE_240X240, 2, xQueueAIFrame);
+    register_camera(PIXFORMAT_RGB565, FRAMESIZE_VGA, 2, xQueueAIFrame);
     register_cat_face_detection(xQueueAIFrame, NULL, xQueueResult, NULL, true);
 
     bool is_detected = false;
@@ -82,11 +93,8 @@ extern "C" void app_main()
         if (xQueueReceive(xQueueResult, &is_detected, portMAX_DELAY) && is_detected) {
             ESP_LOGI("app_main", "Cat face detected!");
 
-            // Get the current time in microseconds
-            int64_t current_time = esp_timer_get_time();
-
             // Check if at least 10 minutes have passed since the last post
-            if (current_time - last_post_time >= TEN_MINUTES_MICROSECONDS) {
+            if (int64_t current_time = esp_timer_get_time(); current_time - last_post_time >= TEN_MINUTES_MICROSECONDS) {
                 httpManager.postToAwsApiGateway(doorbellRingUrl, "");
                 last_post_time = current_time;  // Update the last post time
             } else {
